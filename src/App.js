9fb15e87fc5ad132cd60ebb36c8023c2e8116b49
unknown <hossaina@tcd.ie>
2022-11-08 00:13:49 +0000

    first summary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-function App() {
+//import ExpenseItems from "./components/ExpenseItems";
+import ExpenseRender from "./components/ExpenseRender";
+
+function App() { 
+  const expenses = [
+    {
+      id: 'e1',
+      desc: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      id: 'e2', 
+      desc: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      desc: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      desc: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <ExpenseRender items={expenses}></ExpenseRender>
     </div>
   );
 }
---
+++ src/components/ExpenseRender.js
@@ -0,0 +1,17 @@
+import './ExpenseRender.css'
+import ExpenseItems from "./ExpenseItems";
+import Card from './Card'
+
+function ExpenseRender(props) {
+
+    return(
+        <Card className='expenses'>
+            <ExpenseItems title={props.items[0].desc} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItems>
+            <ExpenseItems title={props.items[1].desc} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItems>
+            <ExpenseItems title={props.items[2].desc} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItems>
+            <ExpenseItems title={props.items[3].desc} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItems>
+        </Card>
+    );
+}
+
+export default ExpenseRender;
