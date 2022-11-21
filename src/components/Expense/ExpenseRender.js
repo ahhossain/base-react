5b658c57a6029b0dcb733b01f0cc3d98cd2554d8
unknown <hossaina@tcd.ie>
2022-11-21 23:19:57 +0000

    completed states and events along with assignment

--- src/components/Expense/ExpenseRender.js
+++ src/components/Expense/ExpenseRender.js
@@ -1,17 +1,29 @@
+import React, {useState} from "react";
 import './ExpenseRender.css'
 import ExpenseItems from "./ExpenseItems";
 import Card from '../Card/Card'
+import ExpensesFilter from './ExpensesFilter';
 
 function ExpenseRender(props) {
 
+    const [year, setYear] = useState('');
+
+    const changeYearData = (yearData) => {
+        setYear(yearData);
+    };
+    
+    console.log(year);
+
     return(
         <Card className='expenses'>
+            <ExpensesFilter selected={year} onChangeYear={changeYearData}/>
             <ExpenseItems title={props.items[0].desc} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItems>
             <ExpenseItems title={props.items[1].desc} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItems>
             <ExpenseItems title={props.items[2].desc} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItems>
             <ExpenseItems title={props.items[3].desc} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItems>
         </Card>
     );
+
 }
 
 export default ExpenseRender;
